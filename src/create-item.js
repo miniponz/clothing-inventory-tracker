4e022cefd0fb4014b4b8d78ea06377e342c41b94
Cara Ponzini <ponzini@gmail.com>
2019-02-08 11:57:14 -0800

    array saved in local storage

---
+++ src/create-item.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    //pull input and store in an object.
+    const name = form.elements.name.value;
+    const category = form.elements.category.value;
+    //console.log(name, category);
+    
+    const singleItem = {
+        name: name,
+        category: category
+    };
+
+    let allItems = [];
+    //empty array
+    const jsonString = window.localStorage.getItem('allItems');
+    if(jsonString){
+        allItems = JSON.parse(jsonString);
+    }
+    // window.localStorage.get item
+    // use .push() to add singleItem to allItems ?
+    allItems.push(singleItem);
+
+    const serialize = JSON.stringify(allItems);
+    window.localStorage.setItem('allItems', serialize);
+    // make jsonString
+    // window.localStorage.set item
+
+    console.log(allItems);
+
+    window.location = 'inventory.html';
+    
+});
+
